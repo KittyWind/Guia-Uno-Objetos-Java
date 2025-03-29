a6d7783831d3a34a17828a3b56485ecdac34fe71
KittyWind <115895672+KittyWind@users.noreply.github.com>
2025-03-29 18:17:11 -0300

    creando clases vacias del ejercicio 13

---
+++ ej13/src/main/java/guiaej13/Guiaej13/Domicilio.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package guiaej13.Guiaej13;
+
+/**
+ *
+ * @author kitty
+ */
+public class Domicilio {
+
+}
