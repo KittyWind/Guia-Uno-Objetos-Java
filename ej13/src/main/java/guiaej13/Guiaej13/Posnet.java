f4a0b366c126835ef3819defd247ec9f9b5b8e59
KittyWind <nicolasasperatti@gmail.com>
2025-04-12 11:35:20 -0300

    modificando posnet de ejercicio 13

--- ej13/src/main/java/guiaej13/Guiaej13/Posnet.java
+++ ej13/src/main/java/guiaej13/Guiaej13/Posnet.java
@@ -14,16 +14,19 @@ public class Posnet {
     private final int PORCENTAJE_POR_CUOTA = 3;
     public Ticket efectuarPagos(TarjetaDeCredito tarjetaDeCredito,double monto,int cuota){
         Ticket ticket = null;
-        if (cuota >= CUOTA_MINIMA_PARA_RECARGO) {
-            monto = monto + ((PORCENTAJE_POR_CUOTA * cuota - 3)/100.0)* monto;
-        }
-        if(tarjetaDeCredito.tieneSaldoSuficiente(monto)){
-            ticket = new Ticket(tarjetaDeCredito.getTitular(),monto/cuota,monto);
-            tarjetaDeCredito.descontarSueldo(monto);
-            System.out.println("pago realizado con exito");
-        }
-        else{
-            System.out.println("pago sin realizar, saldo insuficiente");
+        if(cuota > 0){
+            if (cuota >= CUOTA_MINIMA_PARA_RECARGO) {
+                monto = monto + ((PORCENTAJE_POR_CUOTA * cuota - 3)/100.0)* monto;
+            }
+            if(tarjetaDeCredito.tieneSaldoSuficiente(monto)){
+                ticket = new Ticket(tarjetaDeCredito.getTitular(),monto/cuota,monto);
+                tarjetaDeCredito.descontarSueldo(monto/cuota);
+                System.out.println("pago realizado con exito");
+            }else{
+                System.out.println("pago sin realizar, saldo insuficiente");
+            }
+        }else{
+            System.out.println("pago sin realizar, no puede haber 0 cuotas");
         }
         return ticket;
     }
