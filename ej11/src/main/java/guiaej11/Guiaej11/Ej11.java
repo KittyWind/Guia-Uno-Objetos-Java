4bbf6429f92f0c9d01bf63bdcb177a42c8bd7a35
KittyWind <nicolasasperatti@gmail.com>
2025-04-12 11:25:04 -0300

    creacion de ej11

---
+++ ej11/src/main/java/guiaej11/Guiaej11/Ej11.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package guiaej11.Guiaej11;
+
+/**
+ *
+ * @author kitty10
+ */
+public class Ej11 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
